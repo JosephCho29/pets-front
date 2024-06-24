e31fc232c27383fd13112bd54161034d37fa2789
josephwjcho93@gmail.com <josephcho@Josephs-MacBook-Pro.local>
2024-06-24 15:41:05 -0400

    added upto create function

--- src/services/petService.js
+++ src/services/petService.js
@@ -1,1 +1,29 @@
-BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`;
+
+const index = async () => {
+    try {
+        const res = await fetch(BASE_URL);
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const create = async (formData) => {
+    try {
+        const res = await fetch(BASE_URL, {
+            // We specify that this is a 'POST' request
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export { create };
+export { index };
